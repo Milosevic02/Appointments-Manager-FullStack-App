eb2e7a764c7d52b71edb59af9089c0213ae87688
Dragan <milosevicdragan002@gmail.com>
2024-04-11 20:12:15 +0200

    Get default appointment

---
+++ AppointmentsManagerApp/AppointmentsManager/Controllers/AppointmentController.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using AppointmentsManager.Data;
+using AppointmentsManager.Data.Models;
+
+namespace AppointmentsManager.Controllers
+{
+    [Route("api/appointment")]
+    [ApiController]
+    public class AppointmentController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public AppointmentController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/appointment - default
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Appointment>>> GetAppointments()
+        {
+            if(_context.Appointments != null)
+            {
+                return NotFound();
+            }
+            return await _context.Appointments.Where(e=>!e.Deleted && !e.Done).ToListAsync();
+        }
+
+        // GET: api/appointment/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Appointment>> GetAppointment(int id)
+        {
+            if (_context.Appointments == null)
+            {
+                return NotFound("No Data Found!");
+            }
+            var appointment = await _context.Appointments.FindAsync(id);
+
+            if (appointment == null)
+            {
+                return NotFound("No Data Found!");
+            }
+
+            return appointment;
+        }
+
+        // PUT: api/appointment/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutAppointment(int id, Appointment appointment)
+        {
+            if (id != appointment.ID)
+            {
+                return BadRequest("You are trying to modify the wrong appointment.");
+            }
+
+            //  _context.Entry(appointment).State = EntityState.Modified;
+
+            try
+            {
+
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!AppointmentExists(id))
+                {
+                    return NotFound("The appointment with the Id" + " " + id + " does not exist!");
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return Ok("Appointment updated successfully!");
+        }
+
+        // POST: api/appointment
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Appointment>> PostAppointment(Appointment appointment)
+        {
+            if (_context.Appointments == null)
+            {
+                return Problem("Entity set 'Appointments'  is null.");
+            }
+
+            try
+            {
+                _context.Appointments.Add(appointment);
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException e)
+            {
+                return BadRequest("Could not create the new Appointment: " + e.Message);
+            }
+
+            return CreatedAtAction("GetAppointment", new { id = appointment.ID }, appointment);
+        }
+
+        // DELETE: api/appointment/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteAppointment(int id)
+        {
+            if (_context.Appointments == null)
+            {
+                return NotFound("No Data Found!");
+            }
+            Appointment appointment = await _context.Appointments.FirstAsync(e => e.ID == id);
+            if (appointment == null)
+            {
+                return NotFound("No appointment with the ID " + id);
+            }
+
+            return Ok("Appointment deleted successfully.");
+        }
+
+        private bool AppointmentExists(int id)
+        {
+            return (_context.Appointments?.Any(e => e.ID == id)).GetValueOrDefault();
+        }
+    }
+}
