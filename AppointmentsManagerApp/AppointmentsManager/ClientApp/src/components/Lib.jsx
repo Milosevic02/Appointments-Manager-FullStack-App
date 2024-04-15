591f16061cf02fcb420603b10e8f919532b2ce68
Dragan <milosevicdragan002@gmail.com>
2024-04-15 11:53:33 +0200

    Add style in deleted and done rows

--- AppointmentsManagerApp/AppointmentsManager/ClientApp/src/components/Lib.jsx
+++ AppointmentsManagerApp/AppointmentsManager/ClientApp/src/components/Lib.jsx
@@ -3,4 +3,24 @@ export const testData = [
    { id: 2, Title: "Title two", Description: "Description two", LevelOfImportance: 4, Date: "13-04-2023", Time: "13:32", Address: "Be 9000" },
    { id: 3, Title: "Title three", Description: "Description three", LevelOfImportance: 5, Date: "12-04-2023", Time: "10:32", Address: "Be 2000" },
    { id: 4, Title: "Title four", Description: "Description four", LevelOfImportance: 0, Date: "10-04-2023", Time: "19:09", Address: "Be 1000" },
+
 ]
+
+
+export const entry = {
+    title: "Test title",
+    description: "Test description",
+    address: "Test address",
+    date: new Date(),
+    time: formatedTimeToStr(),
+    done: false,
+    deleted: false,
+    levelOfImportance: 2,
+}
+
+export function formatedTimeToStr(d) {
+    const nd = d ?  new Date(d) : new Date();
+    const hr_ = nd.getHours() < 9 ? 0 + '' + nd.getHours() : nd.getHours()
+    const min_ = nd.getMinutes() < 9 ? 0 + '' + nd.getMinutes() : nd.getMinutes()
+    return hr_ + ':' + min_
+}
--- AppointmentsManagerApp/AppointmentsManager/ClientApp/src/components/Table.jsx
+++ AppointmentsManagerApp/AppointmentsManager/ClientApp/src/components/Table.jsx
@@ -1,50 +1,65 @@
 import React from "react";
 import { DataGrid } from '@mui/x-data-grid';
 import { Button } from "@mui/material";
-
+import "../style/table.css";
 const columns = [
-    { field: 'id', headerName: '#', width: 70 },
+    { field: 'id', headerName: '#', width: 70, align: "center" },
     { field: 'Title', headerName: 'Title', width: 130 },
     { field: 'Description', headerName: 'Description', width: 200 },
-    {field: 'LevelOfImportance',headerName: 'Importance',width: 130,},
-    {field: 'Date',headerName: 'Date',width: 150,},
-    {field: 'Time',headerName: 'Time',width: 100,},
-    {field: 'Address',headerName: 'Address',width: 200,},
-    {field: 'Edit',headerName: 'Edit',width: 130,
-        renderCell:() =>{
-            return(
+    { field: 'LevelOfImportance', headerName: 'Importance', width: 130, align: "center" },
+    { field: 'Date', headerName: 'Date', width: 150 },
+    { field: 'Time', headerName: 'Time', width: 100, align: "center" },
+    { field: 'Address', headerName: 'Address', width: 200 },
+    {
+        field: 'Edit', headerName: 'Edit', width: 130,
+        renderCell: () => {
+            return (
                 <Button
                     variant="contained"
                     color="warning"
                 >Edit</Button>
             )
-        }},
-    {field: 'Delete',headerName: 'Delete',width: 130,
-        renderCell:() =>{
-            return(
+        }
+    },
+    {
+        field: 'Delete', headerName: 'Delete', width: 130,
+        renderCell: () => {
+            return (
                 <Button
                     variant="contained"
                     color="error"
                 >Delete</Button>
             )
-    }},
-  ];
+        }
+    },
+];
 
-const Table = ({dataList}) =>{
-    return(
-        <div style={{ height: 400, width: '100%', paddingLeft:10 }}>
-        <DataGrid
-            rows={dataList}
-            columns={columns}
-            initialState={{
-            pagination: {
-                paginationModel: { page: 0, pageSize: 5 },
-            },
-            }}
-            pageSizeOptions={[5, 10]}
-        />
+const Table = ({ dataList }) => {
+    const getRowClassName = (params) => {
+        if (params.row.deleted) {
+            return 'deleted-row';
+        } else if (params.row.done) {
+            return 'done-row';
+        } else {
+            return 'default-row';
+        }
+    };
+
+    return (
+        <div style={{ height: 400, width: '100%', paddingLeft: 10 }}>
+            <DataGrid
+                rows={dataList}
+                columns={columns}
+                getRowClassName={getRowClassName}
+                initialState={{
+                    pagination: {
+                        paginationModel: { page: 0, pageSize: 5 },
+                    },
+                }}
+                pageSizeOptions={[5, 10]}
+            />
         </div>
     )
 }
 
-export default Table
+export default Table;
