6bb6e991a325e12189107beef13687c9f973c6f2
Dragan <milosevicdragan002@gmail.com>
2024-04-15 11:01:40 +0200

    Input table with testdata

--- AppointmentsManagerApp/AppointmentsManager/ClientApp/src/components/Table.jsx
+++ AppointmentsManagerApp/AppointmentsManager/ClientApp/src/components/Table.jsx
@@ -1,27 +1,40 @@
 import React from "react";
 import { DataGrid } from '@mui/x-data-grid';
+import { Button } from "@mui/material";
 
 const columns = [
     { field: 'id', headerName: '#', width: 70 },
-    { field: 'title', headerName: 'Title', width: 130 },
-    { field: 'description', headerName: 'Description', width: 130 },
-    {field: 'importance',headerName: 'Importance',width: 130,},
-    {field: 'date',headerName: 'Date',width: 130,},
-    {field: 'time',headerName: 'Time',width: 130,},
-    {field: 'address',headerName: 'Address',width: 130,},
-    {field: 'edit',headerName: 'Edit',width: 130,},
-    {field: 'delete',headerName: 'Delete',width: 130,},
-  ];
-  
-  const rows = [
-    
+    { field: 'Title', headerName: 'Title', width: 130 },
+    { field: 'Description', headerName: 'Description', width: 200 },
+    {field: 'LevelOfImportance',headerName: 'Importance',width: 130,},
+    {field: 'Date',headerName: 'Date',width: 150,},
+    {field: 'Time',headerName: 'Time',width: 100,},
+    {field: 'Address',headerName: 'Address',width: 200,},
+    {field: 'Edit',headerName: 'Edit',width: 130,
+        renderCell:() =>{
+            return(
+                <Button
+                    variant="contained"
+                    color="warning"
+                >Edit</Button>
+            )
+        }},
+    {field: 'Delete',headerName: 'Delete',width: 130,
+        renderCell:() =>{
+            return(
+                <Button
+                    variant="contained"
+                    color="error"
+                >Delete</Button>
+            )
+    }},
   ];
 
-const Table = () =>{
+const Table = ({dataList}) =>{
     return(
         <div style={{ height: 400, width: '100%', paddingLeft:10 }}>
         <DataGrid
-            rows={rows}
+            rows={dataList}
             columns={columns}
             initialState={{
             pagination: {
