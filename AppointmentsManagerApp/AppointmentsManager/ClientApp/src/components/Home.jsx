600a115ae43667f2cb33950b93b9595a97bbe509
Dragan <milosevicdragan002@gmail.com>
2024-04-17 18:25:25 +0200

    Add New Component

--- AppointmentsManagerApp/AppointmentsManager/ClientApp/src/components/Home.jsx
+++ AppointmentsManagerApp/AppointmentsManager/ClientApp/src/components/Home.jsx
@@ -3,7 +3,6 @@ import Edit from "./Edit";
 import Delete from "./Delete";
 import New from "./New";
 import { Box, Typography, Tooltip, Fab, Alert, Stack,Item } from "@mui/material";
-import {Add as AddIcon} from "@mui/icons-material"
 import Filter from "./Filter";
 import Table from "./Table";
 import { testData } from "./Lib";
@@ -24,19 +23,10 @@ const Home = () =>{
             <Typography pb={3} textAlign={"center"}>
                 This powerful web application helps you to manage your dates very easy
             </Typography>
-            <Tooltip title="Add Appointments"
-                sx={{
-                    position:"fixed",
-                    bottom:20,
-                    left:{xs:"calc(50% - 25px),md:30"}
-                }}>
-                <Fab color="success" aria-label="add">
-                    <AddIcon/>
-                </Fab>
-            </Tooltip>
             <Alert variant = "filled" severity="success">This is a success Alert.</Alert>
             <Filter pb={3}/>
             <Table dataList={dataList}/>
+            <New/>
         </Box>
     )
 }
