85fdfdf56282e6c92ec0e22b4abd66e946e8b9b0
Dragan <milosevicdragan002@gmail.com>
2024-04-17 16:53:46 +0200

    Add Importance name and color

--- AppointmentsManagerApp/AppointmentsManager/ClientApp/src/components/Table.jsx
+++ AppointmentsManagerApp/AppointmentsManager/ClientApp/src/components/Table.jsx
@@ -1,13 +1,34 @@
 import React from "react";
 import { DataGrid } from '@mui/x-data-grid';
-import { Button } from "@mui/material";
+import { Button,Box } from "@mui/material";
 import "../style/table.css";
+
+
 const columns = [
     { field: 'id', headerName: '#', width: 70, align: "center" },
     { field: 'Title', headerName: 'Title', width: 130 },
     { field: 'Description', headerName: 'Description', width: 200 },
-    { field: 'LevelOfImportance', headerName: 'Importance', width: 130, align: "center" },
-    { field: 'Date', headerName: 'Date', width: 150 },
+    {
+        field: 'LevelOfImportance',
+        headerName: 'Importance',
+        width: 130,
+        align: "center",
+        renderCell: (params) => {
+            let bgColor = ''; // Initialize background color
+            bgColor = params.row.LevelOfImportance === "Very Low" ? 'green' :
+                params.row.LevelOfImportance === "High" ? 'gold' :
+                params.row.LevelOfImportance === "Very High" ? 'red' :
+                     '';
+            return (
+                <Box>
+                    <Box sx={{ backgroundColor: bgColor,borderRadius:"20px"}}>
+                                        {params.value}
+                    </Box>
+                </Box>
+                
+            );
+        }
+    },    { field: 'Date', headerName: 'Date', width: 150 },
     { field: 'Time', headerName: 'Time', width: 100, align: "center" },
     { field: 'Address', headerName: 'Address', width: 200 },
     {
@@ -34,8 +55,34 @@ const columns = [
     },
 ];
 
+
 const Table = ({ dataList }) => {
     const getRowClassName = (params) => {
+        let importance = "";
+        let level = String(params.row.LevelOfImportance);
+        switch (level) {
+            case '0':
+                importance = "Very Low";
+                break;
+            case '1':
+                importance = "Low";
+                break;
+            case '2':
+                importance = "Normal";
+                break;
+            case '3':
+                importance = "Medium";
+                break;
+            case '4':
+                importance = "High";
+                break;
+            case '5':
+                importance = "Very High";
+                break;
+            default:
+                importance = level;
+        }
+        params.row.LevelOfImportance = importance;
         if (params.row.deleted) {
             return 'deleted-row';
         } else if (params.row.done) {
@@ -44,7 +91,6 @@ const Table = ({ dataList }) => {
             return 'default-row';
         }
     };
-
     return (
         <div style={{ height: 400, width: '100%', paddingLeft: 10 }}>
             <DataGrid
