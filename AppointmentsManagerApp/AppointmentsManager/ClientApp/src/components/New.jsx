b2728fde87424bad72814c4c7b9d81a7bdbc8ece
Dragan <milosevicdragan002@gmail.com>
2024-04-22 10:21:47 +0200

    Add letter counter in new comp

--- AppointmentsManagerApp/AppointmentsManager/ClientApp/src/components/New.jsx
+++ AppointmentsManagerApp/AppointmentsManager/ClientApp/src/components/New.jsx
@@ -1,7 +1,7 @@
 import { Box, Fab, Modal, Stack, Button, Tooltip, Typography, styled } from "@mui/material";
 import React, { useState } from "react";
 import {Add as AddIcon} from "@mui/icons-material"
-import {formatedDateToStr,formatedTimeToStr} from "./Lib"
+import {entry, formatedDateToStr,formatedTimeToStr} from "./Lib"
 import CloseIcon from '@mui/icons-material/Close';
 import AddCircleIcon from '@mui/icons-material/AddCircle';
 const StyledModal = styled(Modal)({
@@ -12,7 +12,43 @@ const StyledModal = styled(Modal)({
 
 
 const New = () =>{
+    const [titleLength, setTitleLength] = useState(0)
+    const [desLength, setDesLength] = useState(0)
+    const [addrLength, setAddrLength] = useState(0)
+
+    const newApp = (e) => {
+        const name_ = e.target.name
+        let v_ = e.target.value
+
+        if(name_ === "title"){
+            setTitleLength(v_.length)
+        }
+
+        if(name_ === "description"){
+            setDesLength(v_.length)
+        }
+
+        if(name_ === "address"){
+            setAddrLength(v_.length)
+        }
+
+        if(name_ === "date"){
+            v_ = new Date(v_)
+        }
+
+        if(name_ === "levelOfImportance"){
+            v_ = Number(v_)
+        }
+
+        entry[name_] = v_
+    }
     const [open,setOpen]=useState(false)
+    const handleClose = () => {
+        setOpen(false);
+        setTitleLength(0)
+        setDesLength(0)
+        setAddrLength(0)
+      };
     return(
         <>
             <Tooltip onClick={e=>setOpen(true)}
@@ -35,15 +71,21 @@ const New = () =>{
                     <Box width={1000} height={550} p={3} borderRadius={5} bgcolor={"background.default"} color={"text.primary"}>
                         <Typography variant='h6' color={"gray"} textAlign={"center"}>Create Appointments</Typography>
                         <Typography sx={{fontWeight:"bold",mt:"10px", mb:"5px"}}>Title</Typography>
-                        <input style={{width:"70%"}} type="text" id="Title_n" maxLength={150} name="title"/>
+                        <input style={{width:"70%"}} type="text" id="Title_n" maxLength={150} name="title" onChange={newApp}/>
+                        <span>{titleLength}/150</span>
+
                         <Typography sx={{fontWeight:"bold",mt:"10px", mb:"5px"}}>Description</Typography>
-                        <textarea id="Description_n" maxLength={300} style={{maxHeight:"200px"}}  name="description" cols={102} rows={10}></textarea> <br />
+                        <textarea id="Description_n" maxLength={300} style={{maxHeight:"200px"}}  name="description" onChange={newApp} cols={102} rows={10}></textarea> <br />
+                        <span >{desLength}/300</span>
+                        
                         <Stack direction={"row"} sx={{alignItems:"center", justifyContent:"space-around"}}>
                         
                             <Typography sx={{fontWeight:"bold",mt:"10px", mb:"5px"}}>Address:</Typography>
-                            <input type="text" id="Address_n" name="address" style={{marginLeft:"5px", width:"600px",height:"25px"}} maxLength={100} />
+                            <input type="text" id="Address_n" name="address" style={{marginLeft:"5px", width:"600px",height:"25px"}} maxLength={100} onChange={newApp} />
+                            <span>{addrLength}/100</span>
+
                             <Typography sx={{fontWeight:"bold",mt:"10px", mb:"5px"}}>Importance:</Typography>
-                            <select name="levelOfImportance" id="LevelOfImportance_n" defaultValue={2}>
+                            <select name="levelOfImportance" id="LevelOfImportance_n" onChange={newApp} defaultValue={2}>
                                 <option value={5}>Very High</option>
                                 <option value={4}>High</option>
                                 <option value={3}>Medium</option>
@@ -54,13 +96,13 @@ const New = () =>{
 
                         </Stack>
                         <Stack direction={"row"} sx={{alignItems:"center", justifyContent:""}}>
-                        <Typography sx={{fontWeight:"bold",mt:"10px", mb:"5px",mr:"5px"}}>Date:</Typography>
-                        <input type="date" id="Date_n" name="date" defaultValue={formatedDateToStr()}/>
-                        <Typography sx={{fontWeight:"bold",mt:"10px", mb:"5px", mr:"5px",ml:"10px"}}>Time:</Typography>
-                        <input type="time" id="Time_n" name="time" defaultValue={formatedTimeToStr()}/>
+                            <Typography sx={{fontWeight:"bold",mt:"10px", mb:"5px",mr:"5px"}}>Date:</Typography>
+                            <input type="date" id="Date_n" name="date" onChange={newApp} defaultValue={formatedDateToStr()}/>
+                            <Typography sx={{fontWeight:"bold",mt:"10px", mb:"5px", mr:"5px",ml:"10px"}}>Time:</Typography>
+                            <input type="time" id="Time_n" name="time" onChange={newApp} defaultValue={formatedTimeToStr()}/>
                         </Stack>
                         <Stack direction={"row"} sx={{ml:"20px",mr:"20px",mt:"30px" ,alignItems:"center", justifyContent:"space-between"}}>
-                            <Button startIcon={<CloseIcon />} onClick={e=>setOpen(false)} variant="contained" color="error"sx={{width:"100px"}}>
+                            <Button startIcon={<CloseIcon />} onClick={handleClose} variant="contained" color="error"sx={{width:"100px"}}>
                                 Close
                             </Button>
                             <Button startIcon={<AddCircleIcon />} variant="contained" color="success" sx={{width:"100px"}}>
